97ef577d88d2341996aa55edbad38dabe21893be
AttouAni <anisattouani17@gmail.com>
2024-11-27 00:54:59 +0100

    second

--- models/set.js
+++ models/set.js
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema;
 const setSchema = new Schema({
     reps: { type: Number, required: true },
     weight:{ type: Number, required: true},
-    exercice:{type: Schema.Types.ObjectId, ref:"Exercice"}
+    exercice:{type: Schema.Types.ObjectId, ref:"Exercice"},
+    user:{type: Schema.Types.ObjectId, ref:"User"}
 });
 
 module.exports = mongoose.model("Set", setSchema);
